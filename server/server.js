afe2481b72d4708e6660b216740bd4d2c9c9ac8c
Marcin Gren <marcin.gren@gmail.com>
2018-11-05 21:49:09 +0100

    moved accessToken obtaining logic to a separate file and added callback

--- server/server.js
+++ server/server.js
@@ -10,12 +10,15 @@ const express          = require('express'),
       app              = express(),
       spotifyConnector = require('./spotify-connector'),
       request          = require('request')
+      saveAcessToken   = require('./spotify-connector')
 
 app.use(cors())
 app.options('*', cors())
 app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }))
+
+saveAcessToken()
 
 
 app.get('/api', (req, res) => {
@@ -36,36 +39,20 @@ app.get('/api', (req, res) => {
   // })
 
   // moved from spotify-connector to figure out whats wrong, will refactor after implementing api wrapper
-  const client_id = process.env.CLIENT_ID
-  const client_secret = process.env.CLIENT_SECRET
-  
-    const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(`${client_id}:${client_secret}`).toString('base64'))
-      },
-      form: {
-        grant_type: 'client_credentials'
-      },
-      json: true
-    }
-  
-    request.post(authOptions, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-  
-        const token = body.access_token;
-        const options = {
-          url: 'https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj',
-          headers: {
-            'Authorization': 'Bearer ' + token
-          },
-          json: true
-        }
-        request.get(options, function (error, response, body) {
-          res.send(body)
-        })
-      }
-    })
+  console.log('token321')
+  console.log(accessToken)
+  const options = {
+    url: 'https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    },
+    json: true
+  }
+
+  request.get(options, function (error, response, body) {
+    res.send(body)
+  })
+
 })
 
 app.get('*', (req, res) => {
--- server/spotify-connector.js
+++ server/spotify-connector.js
@@ -1,41 +1,72 @@
 require('dotenv').config()
 const request = require('request') // might change to axios
-const axios = require('axios')
 
-// const {client_id, client_secret} = process.env works fine on server but console in browser throws error?
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
 
-function authenticateSpotify(url) {
+const authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(`${client_id}:${client_secret}`).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+}
 
-  console.log(client_id + 'asd')
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(`${client_id}:${client_secret}`).toString('base64'))
-    },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
+function obtainAccessToken(callback) { request.post(authOptions,  (error, res, body) => {
+  if (!error && res.statusCode === 200) {
+
+    const token = body.access_token
+    callback(null, token)
   }
+})
+}
 
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-
-      const token = body.access_token;
-      const options = {
-        url: url,
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        json: true
-      }
-
-      request.get(options, function (error, response, body) {
-      })
-    }
+function saveAcessToken() {
+  obtainAccessToken((err, res) => {
+    accessToken = res
   })
 }
 
-module.exports = authenticateSpotify
+module.exports = saveAcessToken
+
+
+// // const {client_id, client_secret} = process.env works fine on server but console in browser throws error?
+// const client_id = process.env.CLIENT_ID
+// const client_secret = process.env.CLIENT_SECRET
+
+// function authenticateSpotify(url) {
+
+//   console.log(client_id + 'asd')
+//   const authOptions = {
+//     url: 'https://accounts.spotify.com/api/token',
+//     headers: {
+//       'Authorization': 'Basic ' + (new Buffer(`${client_id}:${client_secret}`).toString('base64'))
+//     },
+//     form: {
+//       grant_type: 'client_credentials'
+//     },
+//     json: true
+//   }
+
+//   request.post(authOptions, function (error, response, body) {
+//     if (!error && response.statusCode === 200) {
+
+//       const token = body.access_token;
+//       const options = {
+//         url: url,
+//         headers: {
+//           'Authorization': 'Bearer ' + token
+//         },
+//         json: true
+//       }
+
+//       request.get(options, function (error, response, body) {
+//       })
+//     }
+//   })
+// }
+
+// module.exports = authenticateSpotify
