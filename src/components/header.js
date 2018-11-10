6e6dc081eab32dd6c2ee127fc1f36c791ab8b271
Marcin Gren <marcin.gren@gmail.com>
2018-11-10 13:03:12 +0100

    Moved header to a separte component

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <div className='header-container'>
+          <h1 className='header-text'>
+            Look for your favourite artist!
+         </h1>
+        </div>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import Arists from './components/artists'
 import SearchBox from './components/search-box'
 import ArtistsList from './components/artists-list'
+import Header from './components/header'
 import artistsStore from './stores/artists-store'
 import './styles/main.scss'
 
 ReactDOM.render(
     <div>
+    <Header />
     <SearchBox artistsStore={artistsStore} />
     {/* <Arists artistsStore={artistsStore} /> */}
     <ArtistsList artistsStore={artistsStore} />
