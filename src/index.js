b06a093bee557a69206f9803f2befad5e1a26686
Marcin Gren <marcin.gren@gmail.com>
2018-11-11 12:37:12 +0100

    removed artists component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Arists from './components/artists'
 import SearchBox from './components/search-box'
 import ArtistsList from './components/artists-list'
 import Header from './components/header'
@@ -11,6 +10,5 @@ ReactDOM.render(
     <div>
     <Header />
     <SearchBox artistsStore={artistsStore} />
-    {/* <Arists artistsStore={artistsStore} /> */}
     <ArtistsList artistsStore={artistsStore} />
 </div>, document.querySelector('.container'))
