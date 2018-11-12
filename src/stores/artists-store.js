ccfec20a976ebbb19864014d93395f0549262384
Marcin Gren <marcin.gren@gmail.com>
2018-11-12 11:58:14 +0100

    removed not used things from aritsts store

--- src/stores/artists-store.js
+++ src/stores/artists-store.js
@@ -1,20 +1,9 @@
-import { autorun, observable, action } from 'mobx'
+import { autorun, observable } from 'mobx'
 
 class ArtistsStore {
   @observable artists = []
   @observable searchedArtist = ''
   @observable foundArtists = []
-
-  addArtist(artist) {
-    this.artists.push({
-      artist: artist
-    })
-  }
-
-  @action
-  updateSearchedArtist(searchedArtist) {
-    this.searchedArtist = searchedArtist
-  }
 }
 
 const artistsStore = new ArtistsStore()
@@ -22,11 +11,7 @@ const artistsStore = new ArtistsStore()
 export default artistsStore
 
 autorun(() => {
-  console.log('artist store searchedArtist is ' + artistsStore.searchedArtist)
-  console.log('artist store foundArtists is ' + JSON.stringify(artistsStore.foundArtists))
-  console.log('artist store foundArtist length is ' + artistsStore.foundArtists.length)
-  
-  if (artistsStore.foundArtists.length > 5) { 
+  if (artistsStore.foundArtists.length > 5) {
     artistsStore.foundArtists.length = 5
   }
 })
