dd043bebc42506145f8c144fe4be6019fc8dfa03
Marcin Gren <marcin.gren@gmail.com>
2018-11-14 17:48:35 +0100

    changed map to replace data not append, handled undef, moved to store

--- src/components/search-box.jsx
+++ src/components/search-box.jsx
@@ -1,37 +1,24 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import { observer } from 'mobx-react'
 
 export default @observer class SearchBox extends Component {
 
   onInputChange = event => {
     const searchedArtist = event.target.value
-
-    axios.get(`/api/artist/${searchedArtist}`)
-      .then(response => {
-        this.props.artistsStore.cleanArtists()
-        response.data.artists.items.map(artist => {
-        // Going to create action in store to handle this I think
-          this.props.artistsStore.foundArtists.unshift({ //todo handle undefineds
-            id: artist.id,
-            name: artist.name,
-            img: artist.images['0'].url
-          })
-        })
-      })
+    this.props.artistsStore.getArtist(searchedArtist)
   }
 
   render() {
 
     return (
       <div className="search-box__input-container">
-          <input
-            className="search-box__artist-input"
-            type="text"
-            name="artist"
-            value={this.searchedArtist}
-            onChange={this.onInputChange}
-          />
+        <input
+          className="search-box__artist-input"
+          type="text"
+          name="artist"
+          value={this.searchedArtist}
+          onChange={this.onInputChange}
+        />
       </div>
     )
   }
--- src/stores/artists-store.js
+++ src/stores/artists-store.js
@@ -1,13 +1,20 @@
 import { observable, action } from 'mobx'
+import axios from 'axios'
 
 class ArtistsStore {
-  @observable searchedArtist = ''
   @observable foundArtists = []
-  @observable artistsInfo
 
   @action
-  cleanArtists() {
-    this.foundArtists = []
+  getArtist(searchTerm) {axios
+    .get(`/api/artist/${searchTerm}`)
+    .then(response => {
+      artistsStore.foundArtists = response.data.artists.items.map(artist => ({
+        id: artist.id,
+        name: artist.name,
+        // url hardcoded for now, going to change later
+        img: artist.images['0'] ? artist.images['0'].url : 'http://089a12354f66f8c089ca-2b0da8e65ee035845e5cc0511bab26ba.r78.cf1.rackcdn.com/global/imagelib/distancelearning/no-img-6c91ea01d3af4559c726afc209ddec79d965cac9.png'
+      }))
+    })
   }
 }
 
