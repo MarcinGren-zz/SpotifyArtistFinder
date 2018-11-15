e319b195b81273b4148c79437e72aeb282068b4d
Marcin Gren <marcin.gren@gmail.com>
2018-11-15 22:14:32 +0100

    added onclick to get artist.id of the given li

--- src/components/artists-list.jsx
+++ src/components/artists-list.jsx
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 
 export default @observer class ArtistsList extends Component {
+
+  onArtistClick(artistId) {
+    console.log(artistId)
+  }
+
   render() {
     const { artistsStore } = this.props
 
     return (
       <ul className="artists-list__list">
-        {/* todo - handle empty */}
         {artistsStore.foundArtists.map(artist => (
           // Going to move li to a separate component later
-          <li key={artist.id} className="artists-list__list-item">
+          <li key={artist.id} className="artists-list__list-item"
+            onClick={() => this.onArtistClick(artist.id)}>
             <figure className="artists-list__item-figure">
               <img src={artist.img} className="artists-list__item-image" />
               <figcaption className="artists-list__item-caption">
