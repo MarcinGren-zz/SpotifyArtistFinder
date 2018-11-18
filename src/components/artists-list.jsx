0bf7c506eb1e96aaaef097396ca3528f75dc4adf
Marcin Gren <marcin.gren@gmail.com>
2018-11-18 19:19:19 +0100

    cr fix: added artist-list-item component with onClick function from al

--- src/components/artists-list.jsx
+++ src/components/artists-list.jsx
@@ -1,13 +1,8 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
+import ArtistsListItem from './artists-list-item'
 
 export default @observer class ArtistsList extends Component {
-
-  onArtistClick(artistId) {
-    this.props.artistInfoStore.clickedArtist = artistId
-    this.props.artistInfoStore.getArtistInfo()
-    this.props.artistInfoStore.displayInfo = true
-  }
 
   render() {
     const { artistsStore } = this.props
@@ -16,15 +11,11 @@ export default @observer class ArtistsList extends Component {
       <ul className="artists-list__list">
         {artistsStore.foundArtists.map(artist => (
           // Going to move li to a separate component later
-          <li key={artist.id} className="artists-list__list-item"
-            onClick={() => this.onArtistClick(artist.id)}>
-            <figure className="artists-list__item-figure">
-              <img src={artist.img} className="artists-list__item-image" />
-              <figcaption className="artists-list__item-caption">
-                {artist.name}
-              </figcaption>
-            </figure>
-          </li>
+          <ArtistsListItem
+            key={artist.id}
+            artist={artist}
+            artistInfoStore={this.props.artistInfoStore}
+          />
         ))}
       </ul>
     )
